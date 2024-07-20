0817047389bb2842dbf62325ee899aeba5bcd06b
MNTadros <bobnakhla@gmail.com>
2024-07-20 12:56:36 -0400

    added preview capabilities to source streams

--- src/render.js
+++ src/render.js
@@ -5,8 +5,9 @@ const stopButton = document.querySelector("stopButton");
 const videoSelectButton = document.getElementById('videoSelectButton');
 videoSelectButton.onclick = getVideoSources;
 
+let mediaRecorder; 
+const recordedChunks = [];
 const { desktopCapturer, remote } = require('electron');
-
 const { Menu } = remote;
 
 async function getVideoSources() {
@@ -25,4 +26,29 @@ async function getVideoSources() {
 
 
   videoOptionsMenu.popup();
+}
+
+async function selectSource(source) {
+  videoSelectButton.innerText = source.name;
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
+    }
+  };
+  // Create a Stream
+  const stream = await navigator.mediaDevices
+  .getUserMedia(constraints);
+
+  // Preview the source in a video element
+  videoElement.srcObject = stream;
+  videoElement.play();
+
+  const options = { mimeType: 'video/webm; codecs=vp9' };
+  mediaRecorder = new MediaRecorder(stream, options);
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop
 }
